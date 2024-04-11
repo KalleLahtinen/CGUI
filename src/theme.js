7b657e19d891adfa73d2982b7d16abb6d085b77d
Kalle Lahtinen <kalle.2.lahtinen@tuni.fi>
2024-04-11 15:33:09 +0300

    Improve theme

--- src/theme.js
+++ src/theme.js
@@ -1,28 +1,18 @@
 import { createTheme } from '@mui/material/styles';
 
 const theme = createTheme({
-palette: {
-    type: 'dark',
-    primary: {
-        main: '#455a64',
-        contrastText: '#ffffff',
-        dark: '#455a64',
-        light: '#cfd8dc',
-    },
-    secondary: {
-        main: '#ad1457',
-    },
-    background: {
-        default: '#8497a0',
-        paper: '#607d8b',
-    },
-    text: {
-        primary: '#f5f5f5',
-    },
-    },
-    typography: {
-    fontFamily: 'Montserrat',
-    fontSize: 14,
+    palette: {
+        mode: 'dark',
+        primary: {
+          main: '#5893df',
+        },
+        secondary: {
+          main: '#2ec5d3',
+        },
+        background: {
+          default: '#416b8e',
+          paper: '#24344d',
+        },
     },
 });
 
